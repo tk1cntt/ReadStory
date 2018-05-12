2bdacc5b9f207c9d68e383bf31b7d02e1db52da1
Tran Anh Tuan <tk1cntt@gmail.com>
2018-05-12 16:04:16 +0700

    Add firebase

--- App/Containers/StoryScreen.js
+++ App/Containers/StoryScreen.js
@@ -1,13 +1,16 @@
 import React, { Component } from 'react'
-import { ScrollView, Text, KeyboardAvoidingView } from 'react-native'
+import { ScrollView, Button, Text, KeyboardAvoidingView, View } from 'react-native'
 import { connect } from 'react-redux'
 // Add Actions - replace 'Your' with whatever your reducer is called :)
 // import YourActions from '../Redux/YourRedux'
 import { Icon } from 'native-base';
 import {
   LoginButton,
+  LoginManager,
   AccessToken
 } from 'react-native-fbsdk';
+
+import firebase from 'react-native-firebase';
 
 // Styles
 import styles from './Styles/StoryScreenStyle'
@@ -18,32 +21,76 @@ class StoryScreen extends Component {
     tabBarIcon: () => <Icon ios='ios-book' android="md-book" style={{fontSize: 24, color: 'white'}}/>
   }
 
+  constructor() {
+    super();
+    this.state = {
+      loading: true,
+    };
+  }
+
+  /**
+   * When the App component mounts, we listen for any authentication
+   * state changes in Firebase.
+   * Once subscribed, the 'user' parameter will either be null 
+   * (logged out) or an Object (logged in)
+   */
+  componentDidMount() {
+    this.authSubscription = firebase.auth().onAuthStateChanged((user) => {
+      this.setState({
+        loading: false,
+        user,
+      });
+    });
+  }
+
+  /**
+   * Don't forget to stop listening for authentication state changes
+   * when the component unmounts.
+   */
+  componentWillUnmount() {
+    this.authSubscription();
+  }
+
+  onLoginOrRegisterFB = () => {
+  LoginManager.logInWithReadPermissions(['public_profile', 'email'])
+    .then((result) => {
+      if (result.isCancelled) {
+        return Promise.reject(new Error('The user cancelled the request'));
+      }
+      // Retrieve the access token
+      return AccessToken.getCurrentAccessToken();
+    })
+    .then((data) => {
+      // Create a new Firebase credential with the token
+      const credential = firebase.auth.FacebookAuthProvider.credential(data.accessToken);
+      // Login with the credential
+      return firebase.auth().signInAndRetrieveDataWithCredential(credential);
+    })
+    .then((user) => {
+      // If you need to do anything with the user, do it here
+      // The user will be logged in automatically by the
+      // `onAuthStateChanged` listener we set up in App.js earlier
+      console.log(user);
+    })
+    .catch((error) => {
+      const { code, message } = error;
+      // For details of error codes, see the docs
+      // The message contains the default Firebase string
+      // representation of the error
+      console.log(error);
+    });
+}
+
   render () {
     return (
-      <ScrollView style={styles.container}>
-        <KeyboardAvoidingView behavior='position'>
-          <Text>HomeScreen</Text>
-          <LoginButton
-            publishPermissions={["publish_actions"]}
-            onLoginFinished={
-              (error, result) => {
-                if (error) {
-                  alert("login has error: " + result.error);
-                } else if (result.isCancelled) {
-                  alert("login is cancelled.");
-                } else {
-                  AccessToken.getCurrentAccessToken().then(
-                    (data) => {
-                      alert(data.accessToken.toString())
-                    }
-                  )
-                }
-              }
-            }
-            onLogoutFinished={() => alert("logout.")} />
-        </KeyboardAvoidingView>
-      </ScrollView>
-    )
+      <View style={styles.container}>
+      <Button
+        onPress={this.onLoginOrRegisterFB}
+        title="Sign in with facebook"
+        color="#3c50e8"
+      />
+      </View>
+    );
   }
 }
 
--- android/app/src/main/java/com/readstory/MainApplication.java
+++ android/app/src/main/java/com/readstory/MainApplication.java
@@ -5,6 +5,10 @@ import android.app.Application;
 import com.facebook.CallbackManager;
 import com.facebook.appevents.AppEventsLogger;
 import com.facebook.react.ReactApplication;
+
+import io.invertase.firebase.RNFirebasePackage;
+import io.invertase.firebase.auth.RNFirebaseAuthPackage;
+
 import com.facebook.reactnative.androidsdk.FBSDKPackage;
 import fm.indiecast.rnaudiostreamer.RNAudioStreamerPackage;
 import com.zmxv.RNSound.RNSoundPackage;
@@ -37,6 +41,8 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new RNFirebasePackage(),
+            new RNFirebaseAuthPackage(),
             new FBSDKPackage(mCallbackManager),
             new RNAudioStreamerPackage(),
             new RNSoundPackage(),
